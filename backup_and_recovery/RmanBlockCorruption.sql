144e9abe02364cad791478dbfed6cc5d1c715514
karlarao <karlarao@gmail.com>
2017-01-11 11:23:14 -0500

    block corruption

---
+++ backup_and_recovery/RmanBlockCorruption.sql
@@ -0,0 +1,7 @@
+select count(*) from V$RECOVER_FILE;
+select count(*) from V$BACKUP_CORRUPTION;
+select count(*) from V$COPY_CORRUPTION;
+select count(*) from V$DATABASE_BLOCK_CORRUPTION;
+
+
+-- select count(*) from V$CORRUPT_XID_LIST;
