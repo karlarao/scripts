8b2ae85665aca6616b7c65c61b6fa9633c18c041
karlarao <karlarao@gmail.com>
2018-09-21 03:43:19 -0400

    fixed px distribution and ash elap avg,min,max

--- performance/planx.sql
+++ performance/planx.sql
@@ -355,21 +355,25 @@ set lines 300
 col sql_exec_start format a30
 col run_time_timestamp format a30
 select sql_id, 
-	  CAST(sql_exec_start AS TIMESTAMP) sql_exec_start,
-      run_time run_time_timestamp, 
+       sql_exec_id,
+       sql_plan_hash_value,
+	   CAST(sql_exec_start AS TIMESTAMP) sql_exec_start,
+       run_time run_time_timestamp, 
  (EXTRACT(HOUR FROM run_time) * 3600
                     + EXTRACT(MINUTE FROM run_time) * 60 
                     + EXTRACT(SECOND FROM run_time)) run_time_sec
 from  (
 select 
        sql_id,
+       sql_exec_id,
+       sql_plan_hash_value,
        max(sql_exec_start) sql_exec_start,
        max(sample_time - sql_exec_start) run_time 
 from 
        gv$active_session_history  
 where sql_id = '&&sql_id.'
 and sql_exec_start is not null 
-group by sql_id,SQL_EXEC_ID
+group by sql_id,sql_exec_id,sql_plan_hash_value
 order by sql_exec_start desc 
 )
 where rownum < 21
@@ -378,7 +382,7 @@ order by sql_exec_start asc
 PRO
 PRO GV$ACTIVE_SESSION_HISTORY - ash_elap exec avg min max 
 PRO ~~~~~~~~~~~~~~~~~~~~~~~~~
-select sql_id,  
+select sql_plan_hash_value,  
 		count(*),
         round(avg(EXTRACT(HOUR FROM run_time) * 3600
                     + EXTRACT(MINUTE FROM run_time) * 60 
@@ -391,18 +395,45 @@ select sql_id,
                     + EXTRACT(SECOND FROM run_time)),2) max 
 from  (
         select 
-               sql_id,
+		       sql_id,
+		       sql_exec_id,
+		       sql_plan_hash_value,
+		       max(sql_exec_start) sql_exec_start,
                max(sample_time - sql_exec_start) run_time
         from 
                gv$active_session_history 
         where
                sql_exec_start is not null 
                and sql_id = '&&sql_id.'
-        group by sql_id,SQL_EXEC_ID
-        order by sql_id 
+        group by sql_id,sql_exec_id,sql_plan_hash_value
        )
-group by sql_id
-order by avg desc
+group by sql_plan_hash_value
+union all
+select  null,
+		count(*),
+        round(avg(EXTRACT(HOUR FROM run_time) * 3600
+                    + EXTRACT(MINUTE FROM run_time) * 60 
+                    + EXTRACT(SECOND FROM run_time)),2) avg , 
+        round(min(EXTRACT(HOUR FROM run_time) * 3600
+                    + EXTRACT(MINUTE FROM run_time) * 60 
+                    + EXTRACT(SECOND FROM run_time)),2) min , 
+        round(max(EXTRACT(HOUR FROM run_time) * 3600
+                    + EXTRACT(MINUTE FROM run_time) * 60 
+                    + EXTRACT(SECOND FROM run_time)),2) max 
+from  (
+        select 
+		       sql_id,
+		       sql_exec_id,
+		       sql_plan_hash_value,
+		       max(sql_exec_start) sql_exec_start,
+               max(sample_time - sql_exec_start) run_time
+        from 
+               gv$active_session_history 
+        where
+               sql_exec_start is not null 
+               and sql_id = '&&sql_id.'
+        group by sql_id,sql_exec_id,sql_plan_hash_value
+       )
 /
 PRO
 PRO GV$ACTIVE_SESSION_HISTORY 
@@ -610,6 +641,7 @@ PRO GV$ACTIVE_SESSION_HISTORY - px distribution
 PRO ~~~~~~~~~~~~~~~~~~~~~~~~~
 break on sql_exec_start on SQL_EXEC_ID on sql_plan_hash_value on dop
 col dop format a10
+col program format a60
 with sql_exec_id_data as (select sql_exec_id,sql_exec_start from
         (
         select 
@@ -627,14 +659,15 @@ select
 	CAST(max(sql_exec_start) AS TIMESTAMP) sql_exec_start, 
 	sql_exec_id, 
 	sql_plan_hash_value, 
+	sql_plan_line_id,
 	case when px_flags is null then 'SERIAL' else 'px'||trunc(px_flags/2097152) end dop, 
 	program, 
 	count(*)
 from gv$active_session_history
 where sql_id = '&&sql_id.'
 and (sql_exec_id,sql_exec_start) in (select sql_exec_id,sql_exec_start from sql_exec_id_data)
-group by sql_exec_id, sql_plan_hash_value, case when px_flags is null then 'SERIAL' else 'px'||trunc(px_flags/2097152) end, program
-order by 1 asc, 6 asc
+group by sql_exec_id, sql_plan_hash_value, sql_plan_line_id, case when px_flags is null then 'SERIAL' else 'px'||trunc(px_flags/2097152) end, program
+order by 1 asc, 4 asc, 6 asc
 /
 PRO
 PRO
