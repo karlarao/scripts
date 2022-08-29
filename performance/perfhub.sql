bb99a9c1e314efa883798e908075320e178377a4
karlarao <karlarao@gmail.com>
2022-08-29 12:31:02 -0400

    perfhub

--- performance/perfhub.sql
+++ performance/perfhub.sql
@@ -1,14 +1,14 @@
 alter session disable parallel query;
 
-set ver off pages 0 linesize 32767 trimspool on trim on long 1000000 longchunksize 10000000
-spool rwp_sqlmon_&&sqlmon_sqlid._perfhub_24hrs_db.html
-select dbms_perf.report_perfhub(is_realtime=>0,type=>'active',outer_start_time=>sysdate-1,selected_start_time=>sysdate-1) from dual;
-spool off
+-- set ver off pages 0 linesize 32767 trimspool on trim on long 1000000 longchunksize 10000000
+-- spool rwp_sqlmon_&&sqlmon_sqlid._perfhub_24hrs_db.html
+-- select dbms_perf.report_perfhub(is_realtime=>0,type=>'active',outer_start_time=>sysdate-1,selected_start_time=>sysdate-1) from dual;
+-- spool off
 
-set ver off pages 0 linesize 32767 trimspool on trim on long 1000000 longchunksize 10000000
-spool rwp_sqlmon_&&sqlmon_sqlid._perfhub_realtime_24hrs_db.html
-select dbms_perf.report_perfhub(is_realtime=>1,type=>'active',outer_start_time=>sysdate-1,selected_start_time=>sysdate-1) from dual;
-spool off
+-- set ver off pages 0 linesize 32767 trimspool on trim on long 1000000 longchunksize 10000000
+-- spool rwp_sqlmon_&&sqlmon_sqlid._perfhub_realtime_24hrs_db.html
+-- select dbms_perf.report_perfhub(is_realtime=>1,type=>'active',outer_start_time=>sysdate-1,selected_start_time=>sysdate-1) from dual;
+-- spool off
 
 set ver off pages 0 linesize 32767 trimspool on trim on long 1000000 longchunksize 10000000
 spool rwp_sqlmon_&&sqlmon_sqlid._perfhub_realtime_12hrs_db.html
@@ -17,7 +17,7 @@ spool off
 
 set ver off pages 0 linesize 32767 trimspool on trim on long 1000000 longchunksize 10000000
 spool rwp_sqlmon_&&sqlmon_sqlid._perfhub_realtime_6hrs_db.html
-select dbms_perf.report_perfhub(is_realtime=>1,type=>'active',outer_start_time=>sysdate-6/24,selected_start_time=>sysdate-6/24) from dual;
+select dbms_perf.report_perfhub(is_realtime=>1,type=>'active',outer_start_time=>sysdate-4/24,selected_start_time=>sysdate-6/24) from dual;
 spool off
 
 set ver off pages 0 linesize 32767 trimspool on trim on long 1000000 longchunksize 10000000
